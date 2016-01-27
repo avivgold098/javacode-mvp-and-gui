4584b03d8cc1570f4418f02b5934609af3be6f2c
aviv <avivgold098@gmail.com>
2016-01-27 12:00:29 +0200

    java project gui with the correct my algo project

---
+++ MVP/src/view/UserChoice.java
@@ -0,0 +1,40 @@
+package view;
+
+import java.util.HashMap;
+
+import algorithms.mazeGenerators.Maze3d;
+import algorithms.mazeGenerators.Position;
+import algorithms.search.Solution;
+import presenter.Command;
+
+public interface UserChoice {
+	/**
+	 * set the view
+	 * @param view
+	 */
+	void setView(View view);
+	/**
+	 * start the thread
+	 */
+	void start();
+	/**
+	 * display the message
+	 * @param message
+	 */
+	void setMessage(String message);
+	/**
+	 * set commands into hash map
+	 * @param hashCommand
+	 */
+	void setHashCommand(HashMap<String, Command> hc);
+	/**
+	 * display the maze
+	 * @param maze
+	 */
+	void setMessage(Maze3d maze);
+	/**
+	 * display the	sol
+	 * @param sol
+	 */
+	void setMessage(Solution<Position> sol);
+}
